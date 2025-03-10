95a57ac7a6188f22e17495e346a70b282762b76d
dragx21 <raghavpareek0321@gmail.com>
2025-03-10 19:50:59 +0530

    10th march

--- src/components/FormValidation.jsx
+++ src/components/FormValidation.jsx
@@ -16,6 +16,8 @@ const Form = () => {
   const [emailColor, setEmailColor] = useState('');
   const [passwordColor, setPasswordColor] = useState('');
   const [confirmColor, setConfirmColor] = useState('');
+  function validate(){}
+
 
   return (
     <div className={styles.main}> 
@@ -25,9 +27,34 @@ const Form = () => {
         </div>
         <form className={styles.box}>
             <input type="text" placeholder='Name' value={user} 
-            className={styles.place} style={{borderColor:userColor}}
+            id={styles.place} style={{borderColor:userColor}}
             onChange={(e) =>setUser(e.target.value)}/>
-            
+            <p className={styles.error}>{errorUser}</p>
+            <input type="email" placeholder='email' value={email} 
+            id={styles.place} style={{borderColor:emailColor}}
+            onChange={(e) =>setEmail(e.target.value)}/>
+            <p className={styles.error}>{errorEmail}</p>
+            <input
+            id={styles.place}
+            type="password"
+            placeholder="Password"
+            style={{ borderColor: passwordColor }}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className={styles.error}>{errorPassword}</p>
+          <input type="password"  id={styles.place} placeholder='confirm Password' 
+          style={{borderColor:confirmColor}} value={confirm}
+          onChange={(e)=>setConfirm(e.target.value)}/>
+          <p className={styles.error}>{errorConfirm}</p>
+           
+           
+          <button className={styles.submitBtn}  onClick={validate} >
+            Submit
+          </button>
+          
+
+          
           </form>
       </div>
     </div>
